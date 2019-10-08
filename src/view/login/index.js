89640b7ad7bf2f3b66ac023a54ca479ae6eeef93
Alex Talha <alex.talha@original.io>
2019-10-08 19:03:45 -0300

    7. Autenticação de usuário com Firebase

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
+import firebase from "../../config/firebase";
+import "firebase/auth";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  function logar() {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, senha)
+      .then(e => alert("Usuario Logado"))
+      .catch(error => alert(error));
+  }
+
   return (
     <div className="login-content d-flex align-items-center">
       <form className="form-signin mx-auto">
         <div className="text-center mb-4">
           <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">
-            Login
+            Logar
           </h1>
         </div>
-
         <input
+          onChange={e => setEmail(e.target.value)}
           type="email"
           id="inputEmail"
           className="form-control my-2"
           placeholder="Email"
         />
-
         <input
+          onChange={e => setSenha(e.target.value)}
           type="password"
           id="inputPassword"
           className="form-control my-2"
           placeholder="Senha"
         />
 
-        <button className="btn btn-lg btn-login btn-block" type="submit">
-          Sign in
+        <button
+          onClick={logar}
+          className="btn btn-lg btn-login btn-block"
+          type="button"
+        >
+          Logar
         </button>
 
         <div className="msg-login text-white text-center my-5">
