1f52fd7062b4944e8a0313f5c55c08da4106e189
Alex Talha <alex.talha@original.io>
2019-10-08 17:14:48 -0300

    5. Desenvolvendo o Layout da Tela de Login

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Login from "../src/view/login";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Login />, document.getElementById("root"));
--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,7 +1,57 @@
 import React from "react";
+import "./index.css";
 
 function Login() {
-  return <h1>Tela de Login </h1>;
+  return (
+    <div className="login-content d-flex align-items-center">
+      <form className="form-signin mx-auto">
+        <div className="text-center mb-4">
+          <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">
+            Login
+          </h1>
+        </div>
+
+        <input
+          type="email"
+          id="inputEmail"
+          className="form-control my-2"
+          placeholder="Email"
+        />
+
+        <input
+          type="password"
+          id="inputPassword"
+          className="form-control my-2"
+          placeholder="Senha"
+        />
+
+        <button className="btn btn-lg btn-login btn-block" type="submit">
+          Sign in
+        </button>
+
+        <div className="msg-login text-white text-center my-5">
+          <span>
+            <strong>WoW!</strong> Você está conectado! &#128526;
+          </span>
+          <br />
+          <span>
+            <strong>Ops!</strong> Verifique se a senha ou usuário estão corretos
+            ! &#128546;
+          </span>
+        </div>
+
+        <div className="opcoes-login mt-5 text-center">
+          <a href="#" className="mx-2">
+            Recuperar Senha
+          </a>
+          <span className="text-white">&#9733;</span>
+          <a href="#" className="mx-2">
+            Quero Cadastrar
+          </a>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
