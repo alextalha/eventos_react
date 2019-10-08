3bc54360f098b6048a67e999b95d8a3bef5e9ab7
Alex Talha <alex.talha@original.io>
2019-10-08 15:50:08 -0300

    4. Criando a View de Login da aplicação

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Login from "../src/view/login";
 
 ReactDOM.render(<App />, document.getElementById("root"));
