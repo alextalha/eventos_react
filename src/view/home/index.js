eaf377418e31c243ad63dcb1f51f148308f0174d
Alex Talha <alex.talha@original.io>
2019-10-20 15:34:37 -0300

    20. Desenvolvendo o componente evento e barra de pesquisa

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -3,12 +3,24 @@ import { useSelector } from "react-redux";
 import "./home.css";
 import { Link } from "react-router-dom";
 import Navbar from "../../componentes/navbar";
+import EventoCard from "../../componentes/evento-card";
 function Home() {
   return (
     <>
       <Navbar />
       <h1>{useSelector(state => state.usuarioEmail)}</h1>
       <h1>{useSelector(state => state.usuarioLogado)}</h1>
+      <div class="row">
+        <EventoCard />
+        <EventoCard />
+        <EventoCard />
+        <EventoCard />
+        <EventoCard />
+        <EventoCard />
+        <EventoCard />
+        <EventoCard />
+        <EventoCard />
+      </div>
     </>
   );
 }
