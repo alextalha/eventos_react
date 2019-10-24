c8e1f4e3b923143119fc8d4da589f7088414836e
Alex Talha <alex.talha@original.io>
2019-10-24 00:23:03 -0300

    ajuste detalhe-eventos

--- src/view/evento-detalhes/index.js
+++ src/view/evento-detalhes/index.js
@@ -9,10 +9,51 @@ function eventoDetalhes() {
   return (
     <>
       <Navbar />
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
-          <h1> Evento Detalhes</h1>
+          <img
+            src="https://via.placeholder.com/150"
+            className="img-banner"
+            alt="banner"
+          />
         </div>
+
+        <div className="row mt-5 d-flex justify-content-around">
+          <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+            <i className="fas fa-ticket-alt fa-2x"></i>
+            <h5>
+              <strong>Tipo</strong>
+            </h5>
+            <span className="mt-3">Festa</span>
+          </div>
+
+          <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+            <i className="fas fa-calendar-alt fa-2x"></i>
+            <h5>
+              <strong>Data</strong>
+            </h5>
+            <span className="mt-3">17/11/1983</span>
+          </div>
+
+          <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+            <i className="fas fa-clock fa-2x"> </i>
+            <h5>
+              <strong>Hora</strong>
+            </h5>
+            <span className="mt-3">22:30</span>
+          </div>
+        </div>
+
+        <div className="row box-detalhes mt-5">
+          <h5 className="mx-auto">
+            <strong>Detalhes do Evento</strong>
+          </h5>
+          <div className="text-justify pt-3">
+            ASASAASDA ASDSADASD ASDASDASDA ASDASDASDAS ASDASDAS A
+          </div>
+        </div>
+
+        <Link to="" className="fas fa-pen-square fa-3x"></Link>
       </div>
     </>
   );
